5596c5c61c1762a714f1762b42f7df62f98c7003
Nikhilesh0026 <nikhilesh7070@gmail.com>
2026-07-24 17:52:32 +0530

    removed code 6

--- sample.java
+++ sample.java
@@ -1,32 +1,30 @@
 public class sample {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
 
         System.out.println("Hello World Nikhilesh");
         sample obj = new sample();
         obj.display(5, 10);
+        obj.multiply(5, 55);
        
     }
 
     public void display(int a, int b) {
         int sum = a + b;
-        System.out.println("Sum: " + sum);
+        System.out.println("The sum of two numbers is: " + sum);
 
-=======
->>>>>>> 1997c6be6f408cf4b0ec7ae735dedaecbf6df7f2
-        System.out.println("Hello World nikhilesh");
+
+
+
     }
 
-<<<<<<< HEAD
+
 
     public void multiply(int a, int b) {
         System.out.println("The multiplication of two numbers is: " + (a * b));
 
-=======
-    {
-        System.out.println("Hello World Gaurav");
->>>>>>> 1997c6be6f408cf4b0ec7ae735dedaecbf6df7f2
     }
+    
 
     public void run() {
         System.out.println("This is a sample method.");
