15a98fd44009ef3ec3e6f96b8eb674176fb972c3
Nikhilesh0026 <nikhilesh7070@gmail.com>
2026-07-24 14:33:06 +0530

    addition Code

--- sample.java
+++ sample.java
@@ -2,14 +2,12 @@ public class sample {
     public static void main(String[] args) {
         System.out.println("Hello World Nikhilesh");
         sample obj = new sample();
-        obj.display();
-        obj.run();
+        obj.display(5, 10);
+       
     }
 
-    public void display() {
-        System.out.println("This is a sample method.");
-    }
-    public void run() {
-        System.out.println("This is a sample method.");
+    public void display(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
