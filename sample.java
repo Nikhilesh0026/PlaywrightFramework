7f932ab3270f81feb5b6c83c64e5f5ac9957e0c8
Nikhilesh0026 <nikhilesh7070@gmail.com>
2026-07-24 14:21:03 +0530

    live changes10

--- sample.java
+++ sample.java
@@ -3,9 +3,13 @@ public class sample {
         System.out.println("Hello World Nikhilesh");
         sample obj = new sample();
         obj.display();
+        obj.run();
     }
 
     public void display() {
+        System.out.println("This is a sample method.");
+    }
+    public void run() {
         System.out.println("This is a sample method.");
     }
 }
